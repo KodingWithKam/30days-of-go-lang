e083893c5f5a35a684a9ffd1a3cedfccaa5cce6f
KamHaze <kodingwithkam@gmail.com>
2023-02-20 13:06:58 -0500

    constants in GO examples

--- helloworld.go
+++ helloworld.go
@@ -14,6 +14,17 @@ import (
 )
 
 func main() {
-	// Simply prints out Hello World when program is run
-	fmt.Println("Hello World!")
+	/*
+		const keyword declares the variable as "constant", which means that it is unchangeable and read-only.
+
+		RULES FOR CONSTANTS:
+		* Constant names follow the same naming rules as variables
+		* Constant names are usually written in uppercase letters (for easy identification and differentiation from variables)
+		* Constants can be declared both inside and outside of a function
+	*/
+	const BIRTH_YEAR = 1995
+	const CURRENT_YEAR = 2023
+
+	// Simply prints out contents when run
+	fmt.Println("Constants", BIRTH_YEAR, CURRENT_YEAR)
 }
